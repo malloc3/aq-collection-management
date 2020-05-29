f82321ca3ec09a582746925a7632121cbe0182c3
Cannon Mallory <56271516+malloc3@users.noreply.github.com>
2020-05-29 13:55:03 -0700

    Aquadoc initial commit

---
+++ js/definitions.js
@@ -0,0 +1,95 @@
+var config = {
+
+  tagline: "The Laboratory</br>Operating System",
+  documentation_url: "http://localhost:4000/aquarium",
+  title: "Collection_Management",
+  navigation: [
+
+    {
+      category: "Overview",
+      contents: [
+        { name: "Introduction", type: "local-md", path: "README.md" },
+        { name: "About this Workflow", type: "local-md", path: "ABOUT.md" },
+        { name: "License", type: "local-md", path: "LICENSE.md" },
+        { name: "Issues", type: "external-link", path: 'https://github.com/malloc3/aq-collection-management/issues' }
+      ]
+    },
+
+    
+
+      {
+
+        category: "Operation Types",
+
+        contents: [
+
+          
+
+        ]
+
+      },
+
+    
+
+    
+
+      {
+
+        category: "Libraries",
+
+        contents: [
+
+          
+            {
+              name: 'CollectionActions',
+              path: 'libraries/CollectionActions' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'CollectionData',
+              path: 'libraries/CollectionData' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'CollectionDisplay',
+              path: 'libraries/CollectionDisplay' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'CollectionLocation',
+              path: 'libraries/CollectionLocation' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'CollectionTransfer',
+              path: 'libraries/CollectionTransfer' + '.html',
+              type: "local-webpage"
+            },
+          
+
+        ]
+
+    },
+
+    
+
+    
+      { category: "Sample Types",
+        contents: [
+          
+        ]
+      },
+      { category: "Containers",
+        contents: [
+          
+        ]
+      }
+    
+
+  ]
+
+};
